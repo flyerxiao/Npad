46a38b947d36827efc152aec5bccd77107c88854
wueiz <vvueiz@gmail.com>
2016-02-15 10:07:24 +0800

    界面 导航条->Bootstrap

--- Npad.py
+++ Npad.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
