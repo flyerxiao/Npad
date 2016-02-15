1512059ac75599cc8a89534852b595494baf1458
wueiz <vvueiz@gmail.com>
2016-02-15 10:39:51 +0800

    功能 初始化数据库 定义数据库模型

--- Npad.py
+++ Npad.py
@@ -5,15 +5,29 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'kfasd(&F(8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 
 class MessageForm(Form):
     message = TextAreaField(u'请留下你的想法', validators=[DataRequired()])
     submit = SubmitField(u'提交')
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.UnicodeText)
+
+    def __repr__(self):
+        return '<Message %r>' % self.name
 
 
 @app.route('/', methods=['GET', 'POST'])
