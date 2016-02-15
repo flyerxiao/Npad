915d0e42cf513b405d5aa423b4cce102d9dbdfed
wueiz <vvueiz@gmail.com>
2016-02-15 10:27:04 +0800

    界面 留言框->wtf

--- Npad.py
+++ Npad.py
@@ -1,13 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'kfasd(&F(8'
 
 
-@app.route('/')
+class MessageForm(Form):
+    message = TextAreaField(u'请留下你的想法', validators=[DataRequired()])
+    submit = SubmitField(u'提交')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    message = None
+    form = MessageForm()
+    if form.validate_on_submit():
+        message = form.message.data
+        form.message.data = ''
+    return render_template('index.html', form=form, message=message)
 
 
 if __name__ == '__main__':
