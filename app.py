fb987a26f29966d67f9fd85fa82ed56695df1022
wueiz <vvueiz@gmail.com>
2016-02-19 15:13:19 +0800

    显示(分离首页和留言,增加留言页,微调模版) 功能(增加留言路由,修改首页路由)

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # 导入各种库和类
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
-from wtforms import TextAreaField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
+from flask_script import Manager, Shell
 
 app = Flask(__name__)
 # wtf 强制加密
 app.config['SECRET_KEY'] = 'kfasd(&F(8'
 # 数据库路径+名字
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
 # 自动提交数据库变动
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.debug = True
@@ -24,9 +24,19 @@ db = SQLAlchemy(app)
 manager = Manager(app)
 
 
+# 注册实例而导入shell
+def make_shell_context():
+    return dict(app=app, db=db, Message=Message)
+
+
+# 为shell命令注册初始化上下文
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
 # 定义留言表格
 class MessageForm(Form):
-    message = TextAreaField(u'请留下你的想法', validators=[DataRequired()])
+    message = StringField(u'请留下你的想法',
+                          validators=[DataRequired(message=u'请留言')])
     submit = SubmitField(u'提交')
 
 
@@ -34,17 +44,31 @@ class MessageForm(Form):
 class Message(db.Model):
     __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.UnicodeText)
+    text = db.Column(db.String)
 
     def __repr__(self):
         return '<Message %r>' % self.text
 
 
 # 定义路由
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    # 初始化留言-设为空
-    message = None
+    # 查询留言列表并赋值
+    messagelist = Message.query.order_by(Message.id.desc()).all()
+    # 定义查询字符串,默认渲染第1页
+    page = request.args.get('page', 1, type=int)
+    # 使用Flask-SQLAlchemy的paginate()方法
+    pagination = Message.query.order_by(Message.id.desc()).paginate(
+        page, per_page=10, error_out=False)
+    # 设置需要分页的项目
+    messagelist = pagination.items
+    # 传递到变量参数到模版
+    return render_template('index.html', messagelist=messagelist,
+                           pagination=pagination)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def add_post():
     # 实例化留言表格
     form = MessageForm()
     # 判断是否成功提交
@@ -58,21 +82,14 @@ def index():
             # 数据库操作,添加刚输入的内容
             db.session.add(message)
         # 重新赋值用来传递给视图
-        message = form.message.data
+        session['message'] = form.message.data
         # 清空留言
         form.message.data = ''
-    # 查询留言列表并赋值
-    messagelist = Message.query.order_by(Message.id.desc()).all()
-    # 定义查询字符串,默认渲染第1页
-    page = request.args.get('page', 1, type=int)
-    # 使用Flask-SQLAlechemy的paginate()方法
-    pagination = Message.query.order_by(Message.id.desc()).paginate(
-        page, per_page=10, error_out=False)
-    # 设置需要分页的项目
-    messagelist = pagination.items
+        return redirect(url_for('add_post'))
+
     # 传递到变量参数到模版
-    return render_template('index.html', form=form, message=message,
-                           messagelist=messagelist, pagination=pagination)
+    return render_template('post.html', form=form,
+                           message=session.get('message'))
 
 
 if __name__ == '__main__':
