03693a0592a29da0d1cfe383d50a468c9f8bfe4b
wueiz <vvueiz@gmail.com>
2016-02-24 11:27:44 +0800

    显示(注册页面,功能(注册表单,注册路由)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp, EqualTo
+from wtforms.validators import ValidationError
+from ..models import User
 
 
 class LoginForm(Form):
@@ -11,3 +13,19 @@ class LoginForm(Form):
         DataRequired(message=u'请输入密码')])
     remember = BooleanField(u'记住我')
     submit = SubmitField(u'登陆')
+
+
+class RegisterForm(Form):
+    name = StringField(u'用户名', validators=[
+        DataRequired(message=u'用户名必须为6-18位大小写字母,数字'), Length(6, 18),
+        Regexp('^[A-Za-z0-9]*$', 0, u'用户名必须为6-18位大小写字母,数字')])
+    password = PasswordField(u'密码', validators=[
+        DataRequired(message=u'请输入8-20位密码'), Length(8, 20)])
+    password2 = PasswordField(u'确认密码', validators=[
+        DataRequired(message=u'请重复输入密码'),
+        EqualTo('password', message=u'密码必须相同')])
+    submit = SubmitField(u'注册')
+
+    def validate_name(self, field):
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError(u'用户名已经存在')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -24,3 +25,14 @@ def logout():
     logout_user()
     flash(u'你已经成功登出')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, password=form.password.data)
+        db.session.add(user)
+        flash(u'注册成功,可以直接登陆了')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
