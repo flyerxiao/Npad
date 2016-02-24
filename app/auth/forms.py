107ba90ad4f4ad11e55419a49a87576614c07a5e
wueiz <vvueiz@gmail.com>
2016-02-24 22:36:04 +0800

    显示(新建用户页,用户页-分页,首页,用户页显示用户名),功能(用户路由,用户页分页,用户页留言过滤查询)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,7 @@ from ..models import User
 
 class LoginForm(Form):
     name = StringField(u'用户名', validators=[
-        DataRequired(message=u'请输入6-18位用户名'), Length(6, 18)])
+        DataRequired(message=u'请输入用户名')])
     password = PasswordField(u'密码', validators=[
         DataRequired(message=u'请输入密码')])
     remember = BooleanField(u'记住我')
--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from flask import render_template, request, session, redirect, url_for
-
+from flask import render_template, request, session, redirect, url_for, abort
 from . import main
 from .forms import TextForm
 from .. import db
-from ..models import Message
+from ..models import Message, User
+from flask_login import current_user
 
 
 @main.route('/')
 def index():
     # 查询留言列表并赋值
-    messagelist = Message.query.order_by(Message.id).all()
+    messagelist = Message.query.order_by(Message.id)
     # 定义查询字符串,默认渲染第1页
     page = request.args.get('page', 1, type=int)
     # 使用Flask-SQLAlchemy的paginate()方法
@@ -35,7 +35,9 @@ def post():
         # 如果不相同的话,执行下列命令
         if text is None:
             # 实例化数据模型,并取出表格中的值,再赋予message
-            newtext = Message(text=form.text.data)
+            newtext = Message(
+                text=form.text.data,
+                user=current_user._get_current_object())
             # 数据库操作,添加刚输入的内容
             db.session.add(newtext)
         # 重新赋值用来传递给视图
@@ -43,4 +45,25 @@ def post():
         return redirect(url_for('main.post'))
     # 传递到变量参数到模版
     return render_template('post.html', form=form,
-                          text=session.get('text'))
+                           text=session.get('text'))
+
+
+@main.route('/user/<name>')
+def user(name):
+    # 查询当前用户的id并赋值给user
+    user = User.query.filter_by(name=name).first()
+    if user is None:
+        abort(404)
+    # 用Message表的外键user_id查询当前用的留言并赋值
+    messagelist = Message.query.filter_by(user_id=user.id)
+    # 定义查询字符串,默认渲染第1页
+    page = request.args.get('page', 1, type=int)
+    # 使用Flask-SQLAlchemy的paginate()方法
+    pagination = Message.query.filter_by(user_id=user.id).paginate(
+        page, per_page=10, error_out=False)
+    # 设置需要分页的项目
+    messagelist = pagination.items
+    # 传递到变量参数到模版
+    return render_template('user.html',
+                           user=user, messagelist=messagelist,
+                           pagination=pagination)
